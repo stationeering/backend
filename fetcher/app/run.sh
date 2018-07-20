4b196159d1f0d3bac428155f42ef9fe709843052
Melair <melair@caldonia.net>
2018-07-20 08:15:22 +0100

    Rework fetcher to listen to steam vdf data and annotate version.ini.

--- fetcher/app/run.sh
+++ fetcher/app/run.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 
 log() {
   echo "Stationeering: $1"
@@ -23,32 +23,56 @@ STEAM_PASSWORD=`echo $value | sed -e 's/^"//' -e 's/"$//'`
 
 log "Loaded credentials for steam user '$STEAM_USERNAME'."
 
-log "Making download directory..."
-mkdir -p /tmp/download
+log "Find steam depot info for Stationeers..."
+/opt/steamcmd/steamcmd.sh "+login anonymous" "+app_info_print 544550" "+quit" > /tmp/stationeers.vdf
 
-log "Copying existing data into it..."
-aws s3 sync s3://stationeering-gamedata/ /tmp/download/
+do_branch() {
+  branch=$1
 
-log "Downloading public branch..."
-mono /opt/depotdownloader/DepotDownloader.exe -app 544550 -beta public -username $STEAM_USERNAME -password $STEAM_PASSWORD -all-platforms -filelist /opt/fetcher/filelist.txt -dir /tmp/download/public/
-abort $? "Failed to download public branch!"
+  value=`aws ssm get-parameter --name "/steam/depot/$branch" --with-decryption --query Parameter.Value`
+  abort $? "Failed to get /steam/depot/$branch"
+  LAST_DEPOT_ID=`echo $value | sed -e 's/^"//' -e 's/"$//'`
 
-log "Downloading beta branch..."
-mono /opt/depotdownloader/DepotDownloader.exe -app 544550 -beta beta -username $STEAM_USERNAME -password $STEAM_PASSWORD -all-platforms -filelist /opt/fetcher/filelist.txt -dir /tmp/download/beta/
-abort $? "Failed to download beta branch!"
+  CURRENT_DEPOT_ID=`node /opt/fetcher/process.js /tmp/stationeers.vdf $branch buildid`
+  abort $? "Failed to read VDF data from Steam."
 
-log "Identifying versions..."
-PUBLIC_VERSION=`cat /tmp/download/public/rocketstation_Data/StreamingAssets/version.ini | grep UPDATEVERSION | awk -F\  '{ printf $2 }' | sed -r 's/\r//g'`
-BETA_VERSION=`cat /tmp/download/beta/rocketstation_Data/StreamingAssets/version.ini | grep UPDATEVERSION | awk -F\  '{ printf $2 }' | sed -r 's/\r//g'`
+  if [ $CURRENT_DEPOT_ID -le $LAST_DEPOT_ID ]; then
+    log "No update for $branch, last depot id $LAST_DEPOT_ID and current depot id $CURRENT_DEPOT_ID."
+    return
+  fi
 
-log "Public: $PUBLIC_VERSION Beta: $BETA_VERSION"
+  CURRENT_DEPOT_LAST_UPDATED=`node /opt/fetcher/process.js /tmp/stationeers.vdf $branch timeupdated`
+  download_dir=/tmp/download/$branch
 
-log "Syncronising public download to S3."
-aws s3 sync /tmp/download/public s3://stationeering-gamedata/public/ --delete --metadata Branch=public,Version=$PUBLIC_VERSION
-abort $? "Failed to public beta branch!"
+  log "Making download directory..."
+  mkdir -p $download_dir
 
-log "Syncronising beta download to S3."
-aws s3 sync /tmp/download/beta s3://stationeering-gamedata/beta/ --delete --metadata Branch=beta,Version=$BETA_VERSION
-abort $? "Failed to sync beta branch!"
+  log "Copying existing data into it..."
+  aws s3 sync s3://stationeering-gamedata/$branch $download_dir
+  
+  log "Downloading $branch from steam..."
+  mono /opt/depotdownloader/DepotDownloader.exe -app 544550 -beta $branch -username $STEAM_USERNAME -password $STEAM_PASSWORD -all-platforms -filelist /opt/fetcher/filelist.txt -dir $download_dir
+  abort $? "Failed to download public branch!"
+
+  log "Identifying versions..."
+  VERSION=`cat $download_dir/rocketstation_Data/StreamingAssets/version.ini | grep UPDATEVERSION | awk -F\  '{ printf $2 }' | sed -r 's/\r//g'`
+  log "Branch: $branch Version: $VERSION"
+
+  log "Annotating version.ini..."
+  mv $download_dir/rocketstation_Data/StreamingAssets/version.ini $download_dir/rocketstation_Data/StreamingAssets/version.ini.orig
+  echo "STEAM_BUILDID=$CURRENT_DEPOT_ID" >> $download_dir/rocketstation_Data/StreamingAssets/version.ini
+  echo "STEAM_TIMEUPDATED=$CURRENT_DEPOT_LAST_UPDATED" >> $download_dir/rocketstation_Data/StreamingAssets/version.ini
+  cat $download_dir/rocketstation_Data/StreamingAssets/version.ini.orig >> $download_dir/rocketstation_Data/StreamingAssets/version.ini
+
+  log "Syncronising $branch download to S3."
+  aws s3 sync $download_dir s3://stationeering-gamedata/$branch/ --delete --metadata Branch=$branch,Version=$VERSION
+  abort $? "Failed to public beta branch!"
+
+  aws ssm put-parameter --name "/steam/depot/$branch" --value $CURRENT_DEPOT_ID --type String
+  abort $? "Failed to update build id in SSM!"
+}
+
+do_branch beta
+do_branch public
 
 log "All done!"
