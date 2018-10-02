becbd232d0b93d28f6d72922f42f4e6e12a360fd
Melair <melair@caldonia.net>
2018-10-02 08:40:36 +0100

    Begin notify lambda for externally triggered checks.

--- notify/index.js
+++ notify/index.js
@@ -1,5 +1,31 @@
 var AWS = require('aws-sdk');
+var SSM = new AWS.SSM();
 
-exports.handler = function (event, context) {
-  context.succeed("Stationeering: Completed.");
-}  
+function log(content) {
+  console.log("Stationeering: " + content);
+}
+
+exports.handler = async function (event, context, callback) {
+  var user = event.parameters.user;
+  var key = event.parameters.key;
+
+  try {
+    var ssmResponse = await SSM.getParameter({ Name: "/notify/user/" + user, WithDecryption: true }).promise();
+
+    if (!ssmResponse.Parameter || ssmResponse.Parameter.Value !== key) {
+      context.succeed("Forbidden");
+      return;
+    }
+  } catch (err) {
+    if (err.code === "ParameterNotFound") {
+      callback(new Error("Forbidden"));
+    } else {
+      log("Failed to fetch parameters from SSM.");
+      log(err);
+      callback(new Error("Failure"));
+    }
+    return;
+  }
+
+  callback(null, { "body": "OK" });
+};
