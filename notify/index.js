b23d045102ef3517262dda6867605047cc81d8c5
Melair <melair@caldonia.net>
2018-10-10 10:24:40 +0100

    Add logging to identify if the lambda is being invoked by a sucessful user.

--- notify/index.js
+++ notify/index.js
@@ -9,15 +9,19 @@ exports.handler = async function (event, context, callback) {
   var user = event.parameters.user;
   var key = event.parameters.key;
 
+  console.log("Notification receieved from '" + user + "'.");
+
   try {
     var ssmResponse = await SSM.getParameter({ Name: "/notify/user/" + user, WithDecryption: true }).promise();
 
     if (!ssmResponse.Parameter || ssmResponse.Parameter.Value !== key) {
-      context.succeed("Forbidden");
+      console.log("User provided incorrect key.");
+      callback(new Error("Forbidden"));
       return;
     }
   } catch (err) {
     if (err.code === "ParameterNotFound") {
+      console.log("User not found.");
       callback(new Error("Forbidden"));
     } else {
       log("Failed to fetch parameters from SSM.");
@@ -26,6 +30,8 @@ exports.handler = async function (event, context, callback) {
     }
     return;
   }
+  
+  console.log("Notification validated from '" + user + "'.");
 
   callback(null, { "body": "OK" });
 };
